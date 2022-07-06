12e61e0787de6923d58c50631acf0d0094ad7d95
insipidX <37544316+insipidX@users.noreply.github.com>
2022-07-07 00:14:05 +0800

    Delete work directory

--- work/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# File:__init__.py.py
-# Author:Jone xie
-# Date:2022/5/19 19:44
-
-$END$
--- work/plot.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# File:plot.py
-# Author:Jone xie
-# Date:2022/5/19 19:44
-
-$END$
